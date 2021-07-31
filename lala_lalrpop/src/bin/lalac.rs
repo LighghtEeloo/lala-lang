8e53e7343e89df1486daa7562814c645a3f58cf2
LighghtEeloo <eelooecho@163.com>
2021-08-01 07:29:58 +0800

    Test. Raw example.

--- lala_lalrpop/src/bin/lalac.rs
+++ lala_lalrpop/src/bin/lalac.rs
@@ -1,10 +1,23 @@
 use lala_lalrpop::lala;
 
 fn main() -> anyhow::Result<()> {
-    
+
     // get the file[s] and combine them in parallel, and then...
 
-    let code = "a:[*]=[b=[];c:[d;e]=[d:=[];e:=[]]]";
+    let code = r#"
+a :[*]= [
+    b = [];
+    c :[d;e]= [
+        d := [];
+        e := [];
+    ];
+    under_score := [];
+    _anony := [];
+    __anony := [];
+    _A_nony := [];
+    _ := [];
+]
+    "#;
     let binder = lala::BindingParser::new().parse(code)?;
     println!("{:#?}", binder);
     Ok(())
