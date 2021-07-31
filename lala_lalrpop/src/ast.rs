e663b6d5cf010677a83007e23fbb26a07509d12d
LighghtEeloo <eelooecho@163.com>
2021-08-01 05:45:28 +0800

    Feat. Support Mask Ast.

--- lala_lalrpop/src/ast.rs
+++ lala_lalrpop/src/ast.rs
@@ -2,12 +2,42 @@ pub type Binder = String;
 
 #[derive(Debug)]
 pub struct Binding {
-    pub name: Binder,
-    pub expose: Option<Vec<Binder>>,
+    pub binder: Binder,
+    pub args: Vec<Binder>,
+    pub mask: Mask,
     pub block: Block
+}
+
+impl From<(Binder, Mask, Block)> for Binding {
+    fn from((binder, mask, block): (Binder, Mask, Block)) -> Self {
+        Binding { binder, args: Vec::new(), mask, block }
+    }
+}
+
+impl From<(Binder, Vec<Binder>, Mask, Block)> for Binding {
+    fn from((binder, args, mask, block): (Binder, Vec<Binder>, Mask, Block)) -> Self {
+        Binding { binder, args, mask, block }
+    }
+}
+
+#[derive(Debug)]
+pub enum Mask {
+    Closed,
+    Exposed {
+        binders: Vec<Binder>
+    },
+    All
 }
 
 #[derive(Debug)]
 pub struct Block {
     pub bindings: Vec<Binding>
+}
+
+impl From<Vec<Binding>> for Block {
+    fn from(bindings: Vec<Binding>) -> Self {
+        Block {
+            bindings
+        }
+    }
 }
