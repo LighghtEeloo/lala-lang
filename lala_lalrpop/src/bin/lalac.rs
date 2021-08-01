b2dfcc16d9d572c6960334e65f05882bb7d14234
LighghtEeloo <eelooecho@163.com>
2021-08-01 11:02:22 +0800

    Test. File and Package.

--- lala_lalrpop/src/bin/lalac.rs
+++ lala_lalrpop/src/bin/lalac.rs
@@ -4,40 +4,52 @@ fn main() -> anyhow::Result<()> {
 
     // get the file[s] and combine them in parallel, and then...
 
-    let code = r#"
-{
-    a :[*]= [
-        b = [];
-        c :[d; e]= [
-            d := [];
-            e := [];
-        ];
-        x : y; z = [
-            y := [];
-            z := [];
-        ];
-        u : * = {
-            v = [];
-            w = [];
-        };
-        f x y := [];
-        g x y : a; b = [
-            a := [];
-            b := [];
-        ];
-        par := {
-            d := [];
-            e := [];
-        };
-        under_score := [];
-        _anony := [];
-        __anony := [];
-        _A_nony := [];
-        _ := [];
-    ]
-}
+    let file_seq = format!("{}", r#"
+a :[*]= [
+    b = [];
+    c :[d; e]= [
+        d := [];
+        e := [];
+    ];
+    x : y; z = [
+        y := [];
+        z := [];
+    ];
+    x' : * = {
+        y' = [];
+        z' = [];
+    };
+    f x y := [];
+    g x y : a; b = [
+        a := [];
+        b := [];
+    ];
+    par := {
+        d := [];
+        e := [];
+    };
+    under_score := [];
+    _anony := [];
+    __anony := [];
+    _A_nony := [];
+    _ := [];
+]
+    "#);
+
+    let res = lala::BindingParser::new().parse(&file_seq).unwrap();
+    println!("{:#?}", res);
+    
+    let fa = r#"
+a := []
     "#;
-    let binder = lala::ParallelParser::new().parse(code)?;
-    println!("{:#?}", binder);
+    let fb = r#"
+b :*= []
+    "#;
+
+    let package_par = format!("{{{};{}}}", fa, fb);
+    
+    let res = lala::ParallelParser::new().parse(&package_par).unwrap();
+    println!("{:#?}", res);
+
     Ok(())
 }
